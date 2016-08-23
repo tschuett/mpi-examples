fec26668455533d0df73442ccf526a1c28a44f2d
Thorsten Schuett <schuett@zib.de>
2016-08-23 22:14:47 +0200

    malloc with memkind(4kb or 2mb)

--- my-malloc.cpp
+++ my-malloc.cpp
@@ -4,11 +4,32 @@
 
 #ifdef HAVE_MEMKIND
 #include <hbwmalloc.h>
+#include <errno.h>
 #endif
 
 #include <cassert>
 
 #ifdef HAVE_MEMKIND
+void *my_malloc(size_t bytes) {
+  void *buffer = nullptr;
+  if(hbw_check_available()) {
+    int res = hbw_posix_memalign_psize(&buffer, 2*1024*1024ULL, bytes,
+				       HBW_PAGESIZE_2MB);
+    if(res == 0)
+      return buffer;
+    else if(res == ENOMEM) {
+      res = hbw_posix_memalign_psize(&buffer, 2*1024*1024ULL, bytes,
+				     HBW_PAGESIZE_4KB);
+      assert(res == 0);
+      return buffer;
+    }
+  } else {
+    int res = posix_memalign(&buffer, 4096, bytes);
+    assert(res == 0);
+
+    return buffer;
+  } 
+}
 #else
 void *my_malloc(size_t bytes) {
   void *buffer = nullptr;
@@ -20,6 +41,9 @@ void *my_malloc(size_t bytes) {
 #endif
 
 #ifdef HAVE_MEMKIND
+void my_free(void *buffer) {
+  hbw_free(buffer);
+}
 #else
 void my_free(void *buffer) {
   free(buffer);
