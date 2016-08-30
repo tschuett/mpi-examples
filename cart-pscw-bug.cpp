58f76e1238149246a7637889e2d74cad59f8425f
Thorsten Schuett <schuett@zib.de>
2016-08-30 20:39:37 +0200

    beautify

--- cart-pscw-bug.cpp
+++ cart-pscw-bug.cpp
@@ -11,21 +11,21 @@ aprun -N24 -n 96 ./cart-pscw.x
 #include <unistd.h>
 
 const int iterations = 100;
-const int message_size = 12*2*3*8*8; // MPI_COMPLEX
-const int window_size = 16 * 6 * (12*2*3*8*8); //bytes
+const int message_size = 12 * 2 * 3 * 8 * 8;           // MPI_COMPLEX
+const int window_size = 16 * 6 * (12 * 2 * 3 * 8 * 8); // bytes
 const int work_time = 500;
 const int longer_time = 1000;
 const int longer_time2 = 0;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   int rank, size;
   MPI_Win win;
   MPI_Group world_group, group;
-  void *baseptr;
+  void* baseptr;
   int dims[3] = {0, 0, 0};
-  int periods[3] = {1,1,1};
+  int periods[3] = {1, 1, 1};
   int group_members[6];
-  void *data[6];
+  void* data[6];
   MPI_Comm comm_cart;
   int xminus, xplus, yminus, yplus, zminus, zplus, rank_source;
 
@@ -35,8 +35,8 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   // init data
-  for(int i = 0; i < 6; i++) {
-    posix_memalign(&data[i], 4096, message_size*16);
+  for (int i = 0; i < 6; i++) {
+    posix_memalign(&data[i], 4096, message_size * 16);
   }
 
   // create topology
@@ -62,10 +62,9 @@ int main(int argc, char **argv) {
   assert(res == 0);
 
   // create window
-  res = MPI_Win_allocate(window_size, 2*sizeof(double), MPI_INFO_NULL,
+  res = MPI_Win_allocate(window_size, 2 * sizeof(double), MPI_INFO_NULL,
                          comm_cart, &baseptr, &win);
   assert(res == 0);
-
 
   // create neighbors group
   res = MPI_Comm_group(MPI_COMM_WORLD, &world_group);
@@ -80,8 +79,7 @@ int main(int argc, char **argv) {
   res = MPI_Group_incl(world_group, 6, group_members, &group);
   assert(res == 0);
 
-
-  for(int i = 0; i < iterations; i++) {
+  for (int i = 0; i < iterations; i++) {
     // post
     res = MPI_Win_post(group, 0, win);
     assert(res == 0);
@@ -93,34 +91,34 @@ int main(int argc, char **argv) {
     assert(res == 0);
 
     // put xminus
-    res = MPI_Put(data[0], message_size, MPI_DOUBLE_COMPLEX, xminus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[0], message_size, MPI_DOUBLE_COMPLEX, xminus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
     // put xplus
-    res = MPI_Put(data[1], message_size, MPI_DOUBLE_COMPLEX, xplus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[1], message_size, MPI_DOUBLE_COMPLEX, xplus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
 
     usleep(work_time); // work
 
     // put yminus
-    res = MPI_Put(data[2], message_size, MPI_DOUBLE_COMPLEX, yminus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[2], message_size, MPI_DOUBLE_COMPLEX, yminus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
     // put yplus
-    res = MPI_Put(data[3], message_size, MPI_DOUBLE_COMPLEX, yplus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[3], message_size, MPI_DOUBLE_COMPLEX, yplus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
 
     usleep(work_time); // work
 
     // put zminus
-    res = MPI_Put(data[4], message_size, MPI_DOUBLE_COMPLEX, zminus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[4], message_size, MPI_DOUBLE_COMPLEX, zminus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
     // put zplus
-    res = MPI_Put(data[5], message_size, MPI_DOUBLE_COMPLEX, zplus, 0, message_size,
-                  MPI_DOUBLE_COMPLEX, win);
+    res = MPI_Put(data[5], message_size, MPI_DOUBLE_COMPLEX, zplus, 0,
+                  message_size, MPI_DOUBLE_COMPLEX, win);
     assert(res == 0);
 
     usleep(longer_time); // work
